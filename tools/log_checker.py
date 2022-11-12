c39bc5f0d2766de42d2073061df71fc81aaa3759
werty144 <anton-paramonov2000@yandex.ru>
2022-11-12 23:40:24 +0100

    log checker for fifo

--- tools/log_checker.py
+++ tools/log_checker.py
@@ -1,8 +1,85 @@
 import os
 
-  
+
+log_folder = os.path.join('..', 'logs')
+
+
+def check_errors():
+	for file in os.listdir(log_folder):
+		if file.endswith('.stderr'):
+			with open(os.path.join(log_folder, file)) as f:
+				content = f.read()
+				if len(content) > 0:
+					print(f'Error in {file}:\n {content}')
+
+
+def get_messages():
+	messages = dict()
+	for file in os.listdir(log_folder):
+		if file.endswith('.output'):
+			process_name = file.split('.')[0]
+			with open(os.path.join(log_folder, file)) as f:
+				messages[process_name] = f.read().splitlines()
+	return messages
+
+
+def check_no_duplication(proc_messages):
+	if not all(len(messages) == len(set(messages)) for messages in proc_messages.values()):
+		print("Violated no duplication")
+		return False
+	return True
+
+
+def check_no_creation(proc_messages, total_number_messages):
+	for proc, messages in proc_messages.items():
+		for message in messages:
+			if message.startswith('d'):
+				content = message.split(' ')[2]
+				if not (1 <= int(content) <= total_number_messages):
+					print(f"Violated no creation on process {proc}")
+					return False
+	return True
+
+
+def check_uniform_agreement(proc_messages):
+	for proc, messages in proc_messages.items():
+		if not all(not message.startswith("d") or all((message in proc_messages[proc]) for proc in proc_messages.keys()) for message in messages):
+			print(f"Violated agreement on process {proc}")
+			return False
+	return True
+
+
+def check_fifo(proc_messages):
+	for proc, messages in proc_messages.items():
+		for i, message in enumerate(messages):
+			if message.startswith("d"):
+				sender, content = message.split(" ")[1:]
+				for j in range(i):
+					if messages[j].startswith("d"):
+						sndr, cont = messages[j].split(" ")[1:]
+						if sender == sndr and int(cont) >= int(content):
+							print(f"Violated fifo on process {proc}")
+							return False
+	return True
+
+
+def check_no_bullshit(proc_messages, n_processes, n_messages):
+	for proc, messages in proc_messages.items():
+		for message in messages:
+			if message.startswith("d"):
+				parts = message.split(" ")
+				if not (1 <= int(parts[1]) <= n_processes) or not (1 <= int(parts[2]) <= n_messages):
+					print(f"Bullshit in {proc}")
+			elif message.startswith("b"):
+				parts = message.split(" ")
+				if not (1 <= int(parts[1]) <= n_messages):
+					print(f"Bullshit in {proc}")
+			else:
+				print(f"Bullshit in {proc}")
+
+
 def main():
-	log_folder = os.path.join('..', 'logs')
+	
 
 	n_messages = -1
 	with open(os.path.join(log_folder, 'config')) as f:
@@ -12,30 +89,14 @@ def main():
 	with open(os.path.join(log_folder, 'hosts')) as f:
 		n_processes = len(f.read().splitlines())
 
-	for file in os.listdir(log_folder):
-		if file.endswith('.stderr'):
-			with open(os.path.join(log_folder, file)) as f:
-				content = f.read()
-				if len(content) > 0:
-					print(f'Error in {file}:\n {content}')
+	proc_messages = get_messages()
 
-	messages = [set() for _ in range(n_processes - 1)]
-
-	with open(os.path.join(log_folder, 'proc01.output')) as f:
-		for line in f.read().splitlines():
-			sender, msg = int(line.split()[1]) - 2, line.split()[2]
-			if msg in messages[sender]:
-				print('Duplicate!')
-			elif not 1 <= int(msg) <= n_messages:
-				print('Creation!')
-			else:
-				messages[sender].add(msg)
-
-	for sender, msgs in enumerate(messages):
-		print(f'{len(msgs)} from {sender + 2}')
-
-	print()
-	print(f'{sum(len(msgs) for msgs in messages)} in total')
+	check_errors()
+	check_no_creation(proc_messages, n_messages)
+	check_no_duplication(proc_messages)
+	check_uniform_agreement(proc_messages)
+	check_fifo(proc_messages)
+	check_no_bullshit(proc_messages, n_processes, n_messages)
 
 
 if __name__ == '__main__':
